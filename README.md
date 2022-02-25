35d49698f061e1b6053a87f45b1977ca78da7691
maxpres <maximilian.pres@web.de>
2022-02-25 10:11:09 +0100

    update

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Quiz App
 
-Quiz app - 10 questions
+This application is a quiz app with 10 questions that can also save your final result in a highscore list. The number of correctly or incorrectly answered questions or the number of skipped questions can be seen in a summary at the end of the quiz.
--- script.js
+++ script.js
@@ -1,81 +1,81 @@
-"use strict";
+'use strict';
 
 const quiz = [
   {
-    q: "Auf wieviele Inseln verteilt sich Indonesien ?",
-    options: ["17.000", "7.000", "23.000", "4.000"],
+    q: 'Auf wieviele Inseln verteilt sich Indonesien ?',
+    options: ['17.000', '7.000', '23.000', '4.000'],
     answer: 0,
   },
   {
-    q: "Die giftigste Schlange der Welt ist die/der ...",
-    options: ["Braunschlange", "Python", "Inlandtaipan", "Seeschlange"],
+    q: 'Die giftigste Schlange der Welt ist die/der ...',
+    options: ['Braunschlange', 'Python', 'Inlandtaipan', 'Seeschlange'],
     answer: 2,
   },
   {
-    q: "Wie oft gewann Deutschland die Fußball-Weltweisterschaft?",
-    options: ["4x", "5x", "6x", "7x"],
+    q: 'Wie oft gewann Deutschland die Fußball-Weltweisterschaft?',
+    options: ['4x', '5x', '6x', '7x'],
     answer: 0,
   },
   {
-    q: "Lieber 0,01€ 30 Tage lang verdoppeln oder 3 Mio € direkt auf die Hand?",
-    options: ["Verdoppeln", "Direkt 5 Mio €"],
+    q: 'Lieber 0,01€ 30 Tage lang verdoppeln oder 3 Mio € direkt auf die Hand?',
+    options: ['Verdoppeln', 'Direkt 5 Mio €'],
     answer: 0,
   },
   {
-    q: "Wie schnell fliegt ein Flugzeug durchschnittlich?",
-    options: ["600 km/h", "900 km/h", "1400 km/h", "1800 km/h"],
+    q: 'Wie schnell fliegt ein Flugzeug durchschnittlich?',
+    options: ['600 km/h', '900 km/h', '1400 km/h', '1800 km/h'],
     answer: 1,
   },
   {
-    q: "Wieviel mal ist Deutschland größer als Holland?",
-    options: ["11 Mal", "9 Mal", "14 Mal", "16 Mal"],
+    q: 'Wieviel mal ist Deutschland größer als Holland?',
+    options: ['11 Mal', '9 Mal', '14 Mal', '16 Mal'],
     answer: 1,
   },
   {
-    q: "Welches dieser Länder hat die meisten Einwohner?",
-    options: ["Australien", "Holland", "Belgien", "Polen"],
+    q: 'Welches dieser Länder hat die meisten Einwohner?',
+    options: ['Australien', 'Holland', 'Belgien', 'Polen'],
     answer: 3,
   },
   {
-    q: "Wieviele Weihnachtsbäume werden in Deutschland jährlich verkauft?",
-    options: ["30 Millionen", "10 Millionen", "So viele wie es Haushalte gibt"],
+    q: 'Wieviele Weihnachtsbäume werden in Deutschland jährlich verkauft?',
+    options: ['30 Millionen', '10 Millionen', 'So viele wie es Haushalte gibt'],
     answer: 0,
   },
   {
-    q: "Welcher Ozean liegt zwischen Europa und Amerika?",
+    q: 'Welcher Ozean liegt zwischen Europa und Amerika?',
     options: [
-      "Indischer Ozean",
-      "Pazifischer Ozean",
-      "Atlantischer Ozean",
-      "Arktischer Ozean",
+      'Indischer Ozean',
+      'Pazifischer Ozean',
+      'Atlantischer Ozean',
+      'Arktischer Ozean',
     ],
     answer: 2,
   },
   {
-    q: "Für welchen Film bekam Leonardo DiCaprio einen Oscar?",
+    q: 'Für welchen Film bekam Leonardo DiCaprio einen Oscar?',
     options: [
-      "The Wolf of Wall Street",
-      "Titanic",
-      "The Revenant",
-      "Django Unchained",
+      'The Wolf of Wall Street',
+      'Titanic',
+      'The Revenant',
+      'Django Unchained',
     ],
     answer: 2,
   },
 ];
 
-const noq = document.querySelector(".number-of-questions");
-const homebox = document.querySelector(".homebox");
-const quizbox = document.querySelector(".quizbox");
-const question = document.querySelector(".question");
-const numberquestion = document.querySelector(".numberquestion");
-const btnNext = document.querySelector(".btn-next");
-const viewResult = document.querySelector(".viewResult");
-const questionContainer = document.querySelector(".question-container");
-const resultBox = document.querySelector(".resultBox");
-const answer = document.querySelectorAll(".answer");
-const optionContainer = document.querySelector(".optionContainer");
-const myAnswers = document.querySelector(".my-answers");
-const btnHighScore = document.querySelector(".btnHighScore");
+const noq = document.querySelector('.number-of-questions');
+const homebox = document.querySelector('.homebox');
+const quizbox = document.querySelector('.quizbox');
+const question = document.querySelector('.question');
+const numberquestion = document.querySelector('.numberquestion');
+const btnNext = document.querySelector('.btn-next');
+const viewResult = document.querySelector('.viewResult');
+const questionContainer = document.querySelector('.question-container');
+const resultBox = document.querySelector('.resultBox');
+const answer = document.querySelectorAll('.answer');
+const optionContainer = document.querySelector('.optionContainer');
+const myAnswers = document.querySelector('.my-answers');
+const btnHighScore = document.querySelector('.btnHighScore');
 
 let questionCounter = -1;
 const availablequestions = [];
@@ -89,14 +89,15 @@ let attempt = 0;
 
 noq.innerText = totalQuestion;
 
-for (let i = 0; i < quiz.length; i++) {
-  availablequestions.push(quiz[i]);
-}
+quiz.forEach((q) => {
+  availablequestions.push(q);
+});
+console.log(availablequestions);
 
 function startQuiz() {
-  homebox.classList.add("hide");
-  quizbox.classList.remove("hide");
-  goBackHomeBtn.classList.add("hide");
+  homebox.classList.add('hide');
+  quizbox.classList.remove('hide');
+  goBackHomeBtn.classList.add('hide');
   getNewQuestion();
   myanswers();
 }
@@ -104,7 +105,7 @@ function startQuiz() {
 function getNewQuestion() {
   questionCounter++;
   numberquestion.innerText =
-    questionCounter + 1 + " of " + totalQuestion + " Questions";
+    questionCounter + 1 + ' of ' + totalQuestion + ' Questions';
   index++;
   currentQuestion = availablequestions[index];
   question.innerText = currentQuestion.q;
@@ -114,52 +115,53 @@ function getNewQuestion() {
   for (let i = 0; i < optionLength; i++) {
     availableOptions.push(i);
   }
+  console.log(optionLength);
+  console.log(availableOptions);
 
-  optionContainer.innerHTML = "";
+  optionContainer.innerHTML = '';
   for (let i = 0; i < optionLength; i++) {
-    const option = document.createElement("div");
+    const option = document.createElement('div');
     option.innerHTML = currentQuestion.options[i];
     option.id = availableOptions[i];
 
     optionContainer.appendChild(option);
-    option.classList.add("option");
-    option.setAttribute("onclick", "getResult(this)");
+    option.classList.add('option');
+    option.setAttribute('onclick', 'getResult(this)');
   }
 
   if (index === quiz.length - 1) {
-    btnNext.classList.add("hide");
-    viewResult.classList.remove("hide");
+    btnNext.classList.add('hide');
+    viewResult.classList.remove('hide');
   }
 }
 
 function removeQuestionContainer() {
-  questionContainer.classList.add("hide");
-  viewResult.classList.add("hide");
-  resultBox.classList.remove("hide");
-  btnHighScore.classList.remove("hide");
-  saveScore.classList.remove("hide");
+  questionContainer.classList.add('hide');
+  viewResult.classList.add('hide');
+  resultBox.classList.remove('hide');
+  btnHighScore.classList.remove('hide');
+  saveScore.classList.remove('hide');
 }
 
 function getResult(element) {
   let id = parseInt(element.id);
 
   if (id === currentQuestion.answer) {
-    element.classList.add("correct");
+    element.classList.add('correct');
     score++;
-    updateAnswerIndicator("correct");
+    updateAnswerIndicator('correct');
 
     console.log(score);
   } else {
-    element.classList.add("wrong");
+    element.classList.add('wrong');
     scoreWrong++;
-    updateAnswerIndicator("wrong");
+    updateAnswerIndicator('wrong');
 
     const optionLen = optionContainer.children.length;
-    // console.log(optionLen);
 
     for (let i = 0; i < optionLen; i++) {
       if (parseInt(optionContainer.children[i].id) === currentQuestion.answer) {
-        optionContainer.children[i].classList.add("correct");
+        optionContainer.children[i].classList.add('correct');
       }
     }
   }
@@ -170,39 +172,37 @@ function unclickableOptions() {
   const optionLen = optionContainer.children.length;
 
   for (let i = 0; i < optionLen; i++) {
-    optionContainer.children[i].classList.add("already-answered");
+    optionContainer.children[i].classList.add('already-answered');
   }
 }
 
 function myanswers() {
-  myAnswers.innerHTML = "";
+  myAnswers.innerHTML = '';
   const totalQuestion = quiz.length;
   for (let i = 0; i < totalQuestion; i++) {
-    const indicator = document.createElement("span");
+    const indicator = document.createElement('span');
     myAnswers.appendChild(indicator);
   }
 }
 
 function updateAnswerIndicator(markType) {
   myAnswers.children[questionCounter].classList.add(markType);
-  // console.log(markType); // right oder wrong (in Element schauen wo es hinzgefügt wird)
-  // console.log(questionCounter);
 }
 
-const totalCorrect = document.querySelector(".total-correct");
-const totalWrong = document.querySelector(".total-wrong");
-const totalScore = document.querySelector(".total-score");
-const totalAttempt = document.querySelector(".total-attempt");
-const percentage = document.querySelector(".percentage");
-const totalQuestions = document.querySelector(".total-question");
-const goBackHomeBtn = document.querySelector(".goBackHome");
+const totalCorrect = document.querySelector('.total-correct');
+const totalWrong = document.querySelector('.total-wrong');
+const totalScore = document.querySelector('.total-score');
+const totalAttempt = document.querySelector('.total-attempt');
+const percentage = document.querySelector('.percentage');
+const totalQuestions = document.querySelector('.total-question');
+const goBackHomeBtn = document.querySelector('.goBackHome');
 
 function res() {
   totalQuestions.innerHTML = quiz.length;
   totalWrong.innerHTML = scoreWrong;
   totalCorrect.innerHTML = score;
   totalScore.innerHTML = score;
-  percentage.innerHTML = (score / totalQuestion) * 100 + " %";
+  percentage.innerHTML = (score / totalQuestion) * 100 + ' %';
   const countAttempt = score + scoreWrong;
 
   if (countAttempt < totalQuestion) {
@@ -210,19 +210,19 @@ function res() {
   } else {
     totalAttempt.innerHTML = 0;
   }
-  goBackHomeBtn.classList.remove("hide");
+  goBackHomeBtn.classList.remove('hide');
 }
 
 function goBackHome() {
-  homebox.classList.remove("hide");
-  resultBox.classList.add("hide");
-  myAnswers.classList.add("hide");
-  quizbox.classList.add("hide");
-  questionContainer.classList.remove("hide");
-  myAnswers.classList.remove("hide");
-  btnNext.classList.remove("hide");
-  highScoresContainer.classList.add("hide");
-  saveScore.classList.add("hide");
+  homebox.classList.remove('hide');
+  resultBox.classList.add('hide');
+  myAnswers.classList.add('hide');
+  quizbox.classList.add('hide');
+  questionContainer.classList.remove('hide');
+  myAnswers.classList.remove('hide');
+  btnNext.classList.remove('hide');
+  highScoresContainer.classList.add('hide');
+  saveScore.classList.add('hide');
   noq.innerText = totalQuestion;
   resetQuiz();
 }
@@ -235,15 +235,15 @@ function resetQuiz() {
   questionCounter = -1;
 }
 
-const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 const maxHighScores = 10;
-const username = document.getElementById("username");
-const save = document.querySelector(".save-btn");
-const highScoresList = document.querySelector(".highScoresList");
-const highScoresContainer = document.querySelector(".highScoresContainer");
-const saveScore = document.querySelector(".saveScore");
+const username = document.getElementById('username');
+const save = document.querySelector('.save-btn');
+const highScoresList = document.querySelector('.highScoresList');
+const highScoresContainer = document.querySelector('.highScoresContainer');
+const saveScore = document.querySelector('.saveScore');
 
-username.addEventListener("keyup", () => {
+username.addEventListener('keyup', () => {
   save.disabled = !username.value; //damit man nicht save score drücken kann wenn kein inhalt da ist
 });
 
@@ -258,10 +258,10 @@ function saveHighScore(e) {
   highScores.sort((a, b) => b.score - a.score);
   highScores.splice(10);
 
-  localStorage.setItem("highScores", JSON.stringify(highScores));
+  localStorage.setItem('highScores', JSON.stringify(highScores));
 
   console.log(highScores);
-  username.value = "";
+  username.value = '';
   window.location.reload();
 }
 
@@ -271,15 +271,15 @@ highScoresList.innerHTML = highScores
       ${scoreHighScore.name}    ${scoreHighScore.score} Points
     </li>`;
   })
-  .join("");
+  .join('');
 
 function viewHighScores() {
-  highScoresContainer.classList.remove("hide");
-  quizbox.classList.add("hide");
-  goBackHomeBtn.classList.remove("hide");
-  homebox.classList.add("hide");
+  highScoresContainer.classList.remove('hide');
+  quizbox.classList.add('hide');
+  goBackHomeBtn.classList.remove('hide');
+  homebox.classList.add('hide');
 }
 
 window.onload = function () {
-  btnHighScore.classList.remove("hide");
+  btnHighScore.classList.remove('hide');
 };
